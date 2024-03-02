13a655b110694a49a4512f1509991763c3bf5c17
younuskhan1 <md.younuskhan412@gmail.com>
2024-03-02 19:45:46 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import "./Navbar.css";
+import PropTypes from 'prop-types'; // ES6
+import logo_light from "../../assets/logo-black.png";
+import logo_dark from "../../assets/logo-white.png";
+import search_icon_light from "../../assets/search-w.png";
+import search_icon_dark from "../../assets/search-b.png";
+import toggle_light from "../../assets/night.png";
+import toggle_dark from "../../assets/day.png";
+
+
+
+const Navbar = ({theme, setTheme}) => {
+
+  const toggle_mode = ()=>{
+    theme === "light" ? setTheme("dark") : setTheme("light");
+   }
+
+    return (
+        <div className="navbar">
+            <img src={theme === "light"? logo_light : logo_dark} alt="" className="logo"/>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Features</li>
+                <li>About</li>
+            </ul>
+
+             <div className="search-box">
+                <input type="text" placeholder="Search" />
+                <img src={theme === "light" ? search_icon_light : search_icon_dark} alt="" />
+             </div>
+
+             <img onClick={()=>{toggle_mode()}} src={theme === "light" ? toggle_light : toggle_dark} alt="" className="toggle-icon"/>
+
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+    theme:PropTypes.string.isRequired,
+    setTheme:PropTypes.func.isRequired,
+}
+export default Navbar;
